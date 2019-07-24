d2335828d246f307e5945b321bb2e9cdb6377588
DLakin01 <DLakin01@gmail.com>
2019-07-24 11:23:31 -0400

    Generating salary list XML

--- transform_to_xml.py
+++ transform_to_xml.py
@@ -48,8 +48,47 @@ def employees_list_xml():
     print("Generated XML")
 
 
+def salary_list_xml():
+    data = pull_data(sql_query)
+    salary_xml = ET.Element("SalaryList")
+    salaries = ET.SubElement(salary_xml, "SalaryRanges")
+
+    for i in range(0, 200000, 10000):
+        employees_in_range = list(filter(lambda x: i - 9999 < int(x[6]) < i, data))
+
+        if not employees_in_range:
+            continue
+        
+        salary_range = ET.SubElement(salaries, "SalaryRange")
+        salary_range.set("name", f"{i - 9999} - {i}")
+
+        for e in employees_in_range:
+            employee = ET.SubElement(salary_range, "empl")
+
+            emp_id = ET.SubElement(employee, "Id")
+            emp_id.text = str(e[0])
+
+            name = ET.SubElement(employee, "FullName")
+            name.text = f"{e[2]} {e[3]}"
+
+            hire_date = ET.SubElement(employee, "HireDate")
+            hire_date.text = '{:%m/%d/%Y}'.format(e[5])
+
+            title = ET.SubElement(employee, "CurrentTitle")
+            title.text = e[8]
+
+            salary = ET.SubElement(employee, "CurrentSalary")
+            salary.text = str(e[6])
+
+    xml = ET.tostring(salary_xml)
+    destination_file = open("/Users/macbook/Desktop/salary_list.xml", "wb")
+    destination_file.write(xml)
+
+    print("Generated XML")
+
 def main():
-    employees_list_xml()
+    #employees_list_xml()
+    salary_list_xml()
 
 if __name__ == "__main__":
     main()
